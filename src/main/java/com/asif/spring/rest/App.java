b7ec6e6565c917a98d33a2b50b8a990f52c760bf
moasifk <logic.asif@gmail.com>
2018-01-09 07:19:55 +0530

    Spring web service example program

---
+++ src/main/java/com/asif/spring/rest/App.java
@@ -0,0 +1,13 @@
+package com.asif.spring.rest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
